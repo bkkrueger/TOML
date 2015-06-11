5f3cfa6ee3c0f3c24fe700c57f6c9e3203897fc7
bkkrueger <bkkrueger@gmail.com>
2015-06-11 11:47:45 -0600

    Added corresponding dump() method.

--- Julia/toml.jl
+++ Julia/toml.jl
@@ -10,4 +10,10 @@ module toml
         return(toml_dict)
     end
 
+    function dump(filename::String, toml_dict::Dict{Any,Any})
+        fout = pyeval(join(["open(\"", filename, "\", 'w')"]))
+        pytoml.dump(fout, toml_dict)
+        fout[:close]()
+    end
+
 end
